b88b73422428fe86f73593f32551ee164b0a6267
Jack Kleeman <jackkleeman@gmail.com>
2024-02-04 18:14:18 +0000

    Use exec instead of spawn, add expression

--- pkl-eval/src/index.ts
+++ pkl-eval/src/index.ts
@@ -1,11 +1,12 @@
 import {getExePath} from "@pkl-community/pkl"
-import {spawn} from "child_process";
+import {execFile} from "child_process";
 
 export type Options = {
   format?: 'json' | 'jsonnet' | 'pcf' | 'properties' | 'plist' | 'textproto' | 'xml' | 'yaml'
   allowedModules?: string[],
   allowedResources?: string[],
   timeout?: number,
+  expression?: string,
 }
 
 export async function evaluate(mod: string, opts?: Options): Promise<string> {
@@ -27,31 +28,29 @@ export async function evaluate(mod: string, opts?: Options): Promise<string> {
     args.push("--timeout", opts.timeout.toString())
   }
 
-  const process = spawn(getExePath(), args, {
+  if (opts?.expression) {
+    args.push("--expression", opts.expression)
+  }
+
+  let resolve: (value: PromiseLike<string> | string) => void;
+  let reject: (err: Error) => void;
+  const out: Promise<string> = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  })
+
+  const process = execFile(getExePath(), args, {
     env: {},
     timeout: opts?.timeout ? (opts?.timeout * 1000) + 100 : undefined,
-  });
-
-  let out = '';
-  let err = '';
-
-  process.stdout.on('data', (data) => {
-    out += data.toString();
-  });
-
-  process.stderr.on('data', (data) => {
-    err += data.toString();
-  });
-
-  let close: Promise<string> = new Promise((resolve, reject) => process.on('close', (code) => {
-    if (code === null || code === 0) {
-      resolve(out)
+  }, (err, stdout, stderr) => {
+    if (err !== null) {
+      reject(new Error(`pkl failed with error ${err} and stderr:\n ${stderr}`));
     } else {
-      reject(new Error(`pkl exited with code ${code} and stderr:\n ${err}`))
+      resolve(stdout)
     }
-  }));
+  });
 
-  await new Promise((resolve, reject) => process.stdin.write(mod, (error) => {
+  await new Promise((resolve, reject) => process.stdin?.write(mod, (error) => {
     if (error) {
       reject(error)
     } else {
@@ -59,7 +58,7 @@ export async function evaluate(mod: string, opts?: Options): Promise<string> {
     }
   }));
 
-  await new Promise(((resolve) => process.stdin.end(resolve)));
+  await new Promise(((resolve) => process.stdin?.end(resolve)));
 
-  return close;
+  return out;
 }
